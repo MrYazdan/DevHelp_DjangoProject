01ee8dd2b15c2b7a41c9defaecdfe59efa655154
mryazdan <mrrezayazdani@yahoo.com>
2021-08-01 17:39:33 +0430

    Create base model and base manager

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+# BaseManager :
+class BaseManager(models.Manager):
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
+    def get_archive(self):
+        return super().get_queryset()
+
+    def get_deleted_list(self):
+        return super().get_queryset().filter(is_deleted=True)
+
+
+# BaseModel :
+class BaseModel(BaseManager):
+    create_time = models.DateTimeField(auto_now_add=True)
+    modify_time = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    objects = BaseManager
+
+    class Meta:
+        abstract = True
