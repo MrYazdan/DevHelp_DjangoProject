d0bd0dab62920ade98ba304778bb614fcb0dea70
mryazdan <mrrezayazdani@yahoo.com>
2021-08-01 20:11:41 +0430

    Initilize views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.http import Http404
+from category.models import Category
+from .models import Product
 
-# Create your views here.
+
+class Products(ListView):
+    template_name = "products_list.html"
+    paginate_by = 8
+
+    def get_queryset(self):
+        return Product.get_active_list()
+
+
+class ProductsByCategory(ListView):
+    template_name = "products_list.html"
+    paginate_by = 8
+
+    def get_queryset(self):
+        print(self.kwargs)
+        category_name = self.kwargs['category_name']
+        category = get_object_or_404(Category.objects.filter(name_en__iexact=category_name))
+        return Product.extra_objects.get_product_by_category(category_name)
+
+
+class ProductDetail(DetailView):
+    template_name = ""
