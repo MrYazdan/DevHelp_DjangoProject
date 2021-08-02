e76c6baf7c36ee5b43556bb79299630a8b984b22
mryazdan <mrrezayazdani@yahoo.com>
2021-08-02 16:54:02 +0430

    Create category partial load

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import partial_footer
+from .views import partial_footer, partial_category, partial_offer
 
 
 urlpatterns = [
-    path('_landing/footer', partial_footer, name="partial_footer")
+    path('_landing/footer', partial_footer, name="partial_footer"),
+    path('_landing/category', partial_category, name="partial_category"),
+    path('_landing/offer', partial_offer, name="partial_offer"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from category.models import Category
 from settings.models import SochialAccount
 from products.models import Product
 
@@ -6,7 +7,7 @@ from products.models import Product
 def partial_footer(request, *args, **kwargs):
     context = {
         "sochials": SochialAccount.objects.all(),
-        # "footer_msg": SiteSettings.objects.all()
+        # "footer_msg": Site.objects.all()
         "footer_msg": "تیم دولوپر کمکی مشتاق نظرات و پیشنهادات شماست :)"
     }
     return render(request, "landing/partial/footer.html", context)
@@ -17,3 +18,10 @@ def partial_offer(request, *args, **kwargs):
         "offers": Product.objects.all().filter(is_offer=True)
     }
     return render(request, "landing/partial/offer.html", context)
+
+
+def partial_category(request, *args, **kwargs):
+    context = {
+        "categories": Category.objects.all()[:12]
+    }
+    return render(request, "landing/partial/category.html", context)
