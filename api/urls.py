323e49e117a9a1aeeddeb4ab652d12e4be7356c0
mryazdan <mrrezayazdani@yahoo.com>
2021-08-06 18:36:33 +0430

    Add url route for addresses and users in api.urls

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('users/', UserListView.as_view()),
     path('products/', ProductListView.as_view()),
+    path('addresses/', AddressListView.as_view()),
     path('categories/', CategoryListView.as_view()),
+    path('users/detail/<int:pk>', UserDetailView.as_view()),
     path('products/detail/<slug:url>', ProductDetailView.as_view()),
+    path('addresses/detail/<int:pk>', AddressDetailView.as_view()),
     path('categories/detail/<slug:url>', CategoryDetailView.as_view()),
 ]
