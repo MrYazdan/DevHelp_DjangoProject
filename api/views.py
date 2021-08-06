6709edcf74ea9fa7d75351b4ba91bf9b575eb207
mryazdan <mrrezayazdani@yahoo.com>
2021-08-06 18:20:45 +0430

    Create user and address serializers views

--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
+from rest_framework import permissions
+
+from api.permissions import IsOwner
 from products.models import Product
 from category.models import Category
 from products.serializers import ProductSerializer
 from category.serializers import CategorySerializer
-from rest_framework.generics import GenericAPIView, mixins
+from rest_framework.generics import GenericAPIView, mixins, ListCreateAPIView, RetrieveUpdateDestroyAPIView, \
+    RetrieveUpdateAPIView
+from core.serializers import UserSerializer, AddressSerializer
+from core.models import User, Address
 
 
 class ProductListView(GenericAPIView, mixins.CreateModelMixin, mixins.ListModelMixin):
@@ -69,3 +75,29 @@ class CategoryDetailView(GenericAPIView, mixins.DestroyModelMixin, mixins.Update
 
     def patch(self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
+
+
+class UserListView(ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAdminUser]
+
+
+class AddressListView(ListCreateAPIView):
+    serializer_class = AddressSerializer
+    queryset = Address.objects.all()
+    permission_classes = [IsOwner]
+
+
+class AddressDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = AddressSerializer
+    queryset = Address.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().filter(owner=self.request.user)
+
+
+class UserDetailView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsOwner]
