ab16176217768d0e89c46702fdf326129b6f14ff
mryazdan <mrrezayazdani@yahoo.com>
2021-08-22 10:12:30 +0430

    Add map to about page

--- settings/urls.py
+++ settings/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import contact
-
 urlpatterns = [
     path('about/', TemplateView.as_view(template_name="landing/about.html"), name="about"),
-    path('contact/', contact, name="contact"),
+    path('contact/', TemplateView.as_view(template_name="landing/contact.html"), name="contact"),
 ]
